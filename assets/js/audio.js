744df047bdbc36fff8e4330a43183a43429f5503
kkumyk <k.kumykova@gmail.com>
2024-03-05 12:08:50 +0000

    fix: rewrite playAudio function to remove id dependency

--- assets/js/audio.js
+++ assets/js/audio.js
@@ -1,5 +1,4 @@
 function playAudio(audioPath) {
-    var playAudio = document.getElementById('playAudio');
-    playAudio.src = audioPath;
-    playAudio.play();
+    let audio = new Audio(audioPath);
+    audio.play();
 }
