d48efefae22b828a5c744291ced250d54a4e6141
kkumyk <k.kumykova@gmail.com>
2024-04-17 15:25:32 +0100

    add explanation comments to the playAudio() function

--- assets/js/alphabet.js
+++ assets/js/alphabet.js
@@ -1,3 +1,23 @@
+/*
+play() method
+- The HTMLMediaElement play() method attempts to begin playback of the media.
+- It returns a Promise object (completion or failure of an asynchronous operation) and its resulting value.
+- The returned Promise is resolved when playback has been successfully started.
+
+Audio() constructor
+- creates and returns a new HTMLAudioElement which can be attached to a document for the user to interact and/or to listen to.
+- a new HTMLAudioElement object, configured to be used for playing back the audio specified by the url.
+
+new
+- the new operator lets lets developers create an instance of a user defined object or of one built-in object types that has a constructor function.
+
+playAudio()
+- playAudio() is a function that plays the audio files that takes one parameter which is the audio path.
+- inside the function, a new Audio object is created using the new keyword and passing audioPath as an argument.
+- the created Audio object - a built-in JS object that allows us to work with audio files - represents the audio we want to play.
+- after the Audio object is created, the play() method is called to play the audio represented by the Audio object.
+*/ 
+
 function playAudio(audioPath) {
     let audio = new Audio(audioPath);
     audio.play();
